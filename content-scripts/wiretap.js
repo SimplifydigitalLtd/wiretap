471ea6e9ef0e60a6968ed167f44699cfb0318c45
Steve Carratt <scarratt@simplifydigital.co.uk>
2015-04-23 16:11:54 +0100

    adding check for require and postal

--- content-scripts/wiretap.js
+++ content-scripts/wiretap.js
@@ -1,11 +1,22 @@
 
-require(['postal'], function (postal) {
+if (postal){
+    tap()
+} else{
+    require(['postal'], function (postal) {
+        tap();
+    });
+}else {
+    console.log('Cannot Wiretap this site and it has neither require nor postal defined.')
+}
 
+
+
+function tap(){
     chrome.runtime.sendMessage(chromeExtensionId, {type: "init"});
 
     postal.addWireTap( function( d, e ) {
         e.timeStamp = e.timeStamp.toString();
         chrome.runtime.sendMessage(chromeExtensionId, {data: e});
     });
-});
+}
 
